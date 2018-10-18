10b1658b1161f9c707dea294588f25419f8533c1
Jordie <jordie@mileshq.net>
2018-10-18 09:29:13 +0100

    Add acc_gyro as dependency

--- src/drive_test.c
+++ src/drive_test.c
@@ -19,5 +19,17 @@ int main(void) {
         wait(300000);
     }
 
+     e_init_port();
+     e_init_motors();
+     int led =0;
+     while (1) {
+         e_set_body_led(led);
+         LED0 = led = led^1;
+        e_set_speed_left(1000);
+        e_set_steps_left(1000);
+        e_set_speed_right(1000);
+        e_set_steps_right(1000);
+        wait(300000);
+    }
     return 0;
 }
