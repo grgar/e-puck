0c62bc054fdbea59bd0da5fb5cdef42d226c04cb
Jordie <jordie@mileshq.net>
2018-10-25 09:33:23 +0100

    Add import for e_agenda

--- src/p2.c
+++ src/p2.c
@@ -1,5 +1,6 @@
 #include "motor_led/advance_one_timer/e_led.h"
 #include "motor_led/advance_one_timer/e_motors.h"
+#include "motor_led/advance_one_timer/e_agenda.h"
 #include "common.h"
 
 void p2_run() {
