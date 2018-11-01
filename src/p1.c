d53b280991d4efad6e89c3fdcbb685f8c0cb1b46
George Garside <grg.garside@gmail.com>
2018-11-01 09:28:15 +0000

    Agenda testing for obstacle avoidance

--- src/p1.c
+++ src/p1.c
@@ -5,19 +5,31 @@
 #include <a_d/advance_ad_scan/e_ad_conv.h>
 #include "common.h"
 
+int ir_front = 2000;
+
+void p1_read() {
+    ir_front = (e_get_calibrated_prox(0) + e_get_calibrated_prox(7)) / 2;
+    if (ir_front < 2000) {
+        e_set_led(0, 1);
+    } else {
+        e_set_led(0, 1);
+    }
+}
+
+void p1_drive() {
+    if (ir_front > 1000) {
+        e_set_speed(50, 0);
+    } else {
+        e_set_speed(0, 0);
+    }
+}
+
 void p1_run() {
     e_init_ad_scan(0);
     e_calibrate_ir();
+    e_activate_agenda(p1_read, 10);
+    e_activate_agenda(p1_drive, 10);
+    e_start_agendas_processing();
     while (1) {
-        int i;
-        for (i = 0; i < 8; i++) {
-            unsigned int light = e_get_calibrated_prox(i);
-            if (light < 2000) {
-                e_set_led(i, 1);
-            } else {
-                e_set_led(i, 0);
-            }
-        }
-        delay(100000);
     }
 }
