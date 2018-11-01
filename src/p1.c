01ec9088d7cd3427094d91c60cd416cd4b7a8afe
George Garside <grg.garside@gmail.com>
2018-11-01 15:36:13 +0000

    Direction adjustment to avoid walls not in front

--- src/p1.c
+++ src/p1.c
@@ -6,36 +6,82 @@
 #include <a_d/advance_ad_scan/e_ad_conv.h>
 #include "common.h"
 
-int ir_front = 2000;
+// IR percentages, 100% clear, 0% blocked
+int ir[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+int ir_front = 0;
 
-void p1_read() {
-    ir_front = max(e_get_calibrated_prox(0), e_get_calibrated_prox(7));
-    if (ir_front < 3000) {
-        e_set_led(3, 1);
+void p1_ir() {
+    int i;
+    for (i = 0; i < 8; i++) {
+        ir[i] = 100 - (min(e_get_calibrated_prox(i), 3000) / 30);
+    }
+    ir_front = max(ir[0], ir[7]);
+}
+
+int direction_adjust = 0;
+
+void p1_obstacle() {
+    if (ir_front > 85) {
+        return;
+    }
+    
+    /*if (!(ir[1] > 85 || ir[6] > 85)) {
+        // Trapped, reverse
+        e_set_speed(-450, (ir[1] > ir[6]) ? 100 : -100);
+        return;
+    }*/
+
+    if (ir[0] > ir[7]) {
+        // Turn left
+        e_set_speed(0, -1000);
+        return;
     } else {
-        e_set_led(3, 0);
+        // Turn right
+        e_set_speed(0, 1000);
+        return;
     }
 }
 
 void p1_drive() {
-    if (ir_front < 500) {
-        e_set_speed(350, 0);
-    } else {
-        if (e_get_calibrated_prox(0) < e_get_calibrated_prox(7)) {
-            // Turn left
-            e_set_speed(0, -1000);
-        } else {
-            // Turn right
-            e_set_speed(0, 1000);
-        }
+    if (ir_front > 85) {
+        e_set_speed(350, direction_adjust);
+    }
+}
+
+void p1_direction() {
+    if (!(ir[1] < 85 || ir[2] < 85 || ir[5] < 85 || ir[6] < 85)) {
+        direction_adjust = 0;
+        return;
+    }
+    
+    // Front-side
+    if (ir[1] < 85) {
+        direction_adjust = 100;
+    }
+    if (ir[6] < 85) {
+        direction_adjust = -100;
+    }
+    if (ir[1] < 85 || ir[6] < 85) {
+        return;
+    }
+
+    // Side
+    if (ir[2] < 85) {
+        direction_adjust = 50;
+    }
+    if (ir[5] < 85) {
+        direction_adjust = -50;
     }
 }
 
 void p1_run() {
     e_init_ad_scan(0);
     e_calibrate_ir();
-    e_activate_agenda(p1_read, 1000);
+    e_activate_agenda(p1_ir, 500);
+    delay(50000);
     e_activate_agenda(p1_drive, 1000);
+    e_activate_agenda(p1_obstacle, 1000);
+    e_activate_agenda(p1_direction, 1000);
     e_start_agendas_processing();
     while (1) {
     }
