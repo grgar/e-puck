834a1dd4d3984bc161186daf4784cfe56249b8ad
Jordie <jordie@mileshq.net>
2018-11-02 17:36:03 +0000

    Add movement for p3 WITHOUT delay

--- src/p3.c
+++ src/p3.c
@@ -1,4 +1,16 @@
+#include "motor_led/advance_one_timer/fast_agenda/e_led.h"
+#include "motor_led/advance_one_timer/fast_agenda/e_motors.h"
 #include "common.h"
+#include "motor_led/advance_one_timer/fast_agenda/e_agenda_fast.h"
+
 
 void p3_run() {
+    int led = 0;
+    e_set_speed_left(1000);
+    e_set_speed_right(1000);
+    while (1) {
+        led = led^1;
+        e_set_body_led(led);
+        e_set_led(0, led);
+    }
 }
