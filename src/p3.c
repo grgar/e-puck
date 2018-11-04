7a1deacd023536da19f68cce08e1b904d2ac8148
Jordie <jordie@mileshq.net>
2018-11-04 16:29:00 +0000

    Add move if senses light

--- src/p3.c
+++ src/p3.c
@@ -3,7 +3,6 @@
 #include "common.h"
 #include "motor_led/advance_one_timer/fast_agenda/e_agenda_fast.h"
 #include "a_d/advance_ad_scan/e_prox.h"
-#include "motor_led/advance_one_timer/e_led.h"
 
 int sensors[8] = {0};
 
@@ -12,6 +11,16 @@ void p3_sense() {
     for (i = 0; i < 8; i++) {
         sensors[i] = 5000 - (e_get_ambient_light(i));
     }
+}
+
+int p3_any_sensor_on(){
+    int i;
+    for (i=0; i < 8; i++){
+        if (sensors[i] > 1120){
+            return 1;
+        }
+    }
+    return 0;
 }
 
 void p3_run() {
@@ -26,6 +35,8 @@ void p3_run() {
         for (i = 0; i < 8; i++) {
             if (sensors[i] > 1120) {
                 e_set_led(i, 1);
+                e_set_speed_left(1000);
+                e_set_speed_right(1000);
             } else {
                 e_set_led(i, 0);
             }
@@ -34,6 +45,10 @@ void p3_run() {
             } else {
                 e_set_front_led(0);
             }
+        }
+        if(p3_any_sensor_on() == 0){
+             e_set_speed_left(0);
+             e_set_speed_right(0);
         }
 
 
