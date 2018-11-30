0a73e8b115b7d9ba36d531fdac489630ee603421
Toby Mellor <me@tobymellor.com>
2018-11-30 14:17:18 +0000

    feat: find a gap

--- src/p6.c
+++ src/p6.c
@@ -1,14 +1,14 @@
-#include "motor_led/e_init_port.h"
-#include "motor_led/e_epuck_ports.h"
-#include "motor_led/advance_one_timer/e_motors.h"
-#include "motor_led/advance_one_timer/e_agenda.h"
-#include "uart/e_uart_char.h"
-#include "motor_led/advance_one_timer/e_led.h"
-#include "camera/fast_2_timer/e_poxxxx.h"
+#include <motor_led/e_init_port.h>
+#include <motor_led/e_epuck_ports.h>
+#include <motor_led/advance_one_timer/fast_agenda/e_led.h>
+#include <motor_led/advance_one_timer/fast_agenda/e_motors.h>
+#include <motor_led/advance_one_timer/fast_agenda/e_agenda_fast.h>
+#include <uart/e_uart_char.h>
+#include <camera/fast_2_timer/e_poxxxx.h>
 
-#include "stdio.h"
-#include "string.h"
-#include "stdlib.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 #include "./p6.h"
 #include <e_uart_char.h>
@@ -16,6 +16,38 @@
 typedef int bool;
 #define true 1
 #define false 0
+
+// TODO: Move to common (this is George's code)
+typedef struct {
+    /**
+     * IR percentages, 100% clear, 0% blocked
+     */
+    int val[8];
+} p6_IR;
+
+p6_IR p6_ir = {.val =
+    { 0}};
+
+// TODO: Move to common (this is George's code)
+void p6_sense() {
+    int i;
+    int val[8] = {0};
+
+    for (i = 0; i < 8; i++) {
+        val[i] = max(100 - (min(e_get_calibrated_prox(i), 3000) / 30), 0);
+    }
+
+    for (i = 0; i < 8; i++) {
+        if (val[i] < 80 && min(val[(i + 2) % 8] < 80, val[(i + 3) % 8] < 80)) {
+            val[(i + 1) % 8] = 80;
+            if (val[(i + 3) % 8] < val[(i + 2) % 8]) {
+                val[(i + 2) % 8] = 80;
+            }
+        }
+    }
+
+    memcpy(p1_ir.val, val, sizeof val);
+}
 
 void p6_drive() {
     e_set_speed(1000, 0);
@@ -25,33 +57,62 @@ void p6_stop() {
     e_set_speed(0, 0);
 }
 
-void p6_consider_stopping() {
-    int steps = abs(e_get_steps_left() + e_get_steps_right());
-   
-    e_init_uart1();
-    char data[4] = "In\n";
-    e_send_uart1_char(data, 4);
-        
-    e_init_uart1();
-    char message[50];
-    sprintf(message, "This is a test: %d\n", steps);
-    e_send_uart1_char(message, strlen(message));
-    
-    if (steps > 1000) {
-        e_init_uart1();
-        char data[7] = "hello\n";
-        e_send_uart1_char(data, 7);
+int p6_get_steps() {
+    return abs(e_get_steps_left()) + abs(e_get_steps_right());
+}
 
-        p6_stop();
+bool p6_is_gap() {
+    int DISTANCE_THRESHOLD = 0.5; // How far away we should consider "a gap"
+    
+    return p6_IR[5] >= DISTANCE_THRESHOLD;
+}
+
+bool p6_is_big_gap() {
+    int gapStart = p6_get_steps();
+    int MINIMUM_GAP_SIZE = 1000;
+    
+    while (1) {
+        bool isEndOfGap = !p6_is_gap();
+        
+        if (isEndOfGap) {
+            return false;
+        }
+        
+        int currentGapSize = p6_get_steps();
+        int gapSize = currentGapSize - gapStart;
+        
+        if (gapSize >= MINIMUM_GAP_SIZE) {
+            return true;
+        }
     }
 }
 
 void p6_run(void) {
+    e_init_uart1();
+    e_activate_agenda(p6_sense, 500);
     p6_drive();
     
-    e_activate_agenda(p6_consider_stopping, 500);
-    
 	while (1) {
+        bool isGap = p6_is_gap();
+
+        // If it's the start of an opening
+        if (isGap) {
+            bool isBigGap = p6_is_big_gap();
+
+            // Stop if it's a big enough gap to fit the ePuck
+            if (isBigGap) {
+                p6_stop();
+                break;
+            }
+        }
+
+        int steps = p6_get_steps();
+
+        // If this is taking too long, stop anyway
+        if (steps > 5000) {
+            p6_stop();
+            break;
+        }
     }
 }
 
