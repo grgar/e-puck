034b8899ee75eb4f8823d5a0f0a1c9afa767e99d
George Garside <grg.garside@gmail.com>
2018-12-06 10:51:07 +0000

    p10 combining p1 and p5

--- src/p10.c
+++ src/p10.c
@@ -2,9 +2,23 @@
 #include <motor_led/advance_one_timer/fast_agenda/e_led.h>
 #include <motor_led/advance_one_timer/fast_agenda/e_motors.h>
 #include <motor_led/advance_one_timer/fast_agenda/e_agenda_fast.h>
-#include <common.h>
+#include "common.h"
+#include "p1.h"
+#include "p5.h"
+
+void p10_goal_obstacle() {
+    p1_v = p1_obstacle(p5_move_towards_goal());
+}
 
 void p10_run() {
-    e_init_port();
-    e_init_motors();
+    e_set_steps_left(0);
+    e_set_steps_right(0);
+
+    p5_set_goal(50, 50);
+
+    e_activate_agenda(p1_sense, 500);
+    e_activate_agenda(p1_drive, 500);
+    e_activate_agenda(p10_goal_obstacle, 500);
+    while (1) {
+    }
 }
